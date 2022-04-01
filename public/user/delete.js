c9cde41d1b8da90235487244e4ac6b78aeeb62b3
LAPTOP-7KGKGCAR\Sandr <sandrakaljula@hotmail.com>
2022-04-01 10:59:52 +0200

    fixed delete

---
+++ public/user/delete.js
@@ -0,0 +1,34 @@
+// Forms
+const deleteForm = document.querySelector('#delete-form');
+
+// delete input id
+const deleteId = document.querySelector('#delete-id');
+
+// Error messages
+const deleteErrorMsg = document.querySelector('#delete-error');
+
+// Search results
+const deleteResult = document.querySelector('#delete-result');
+
+// Search for user by id
+deleteForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const deleteDetails = {
+        id: deleteId.value,
+    };
+
+    fetch(`/api/users/${deleteDetails.id}`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(deleteDetails)
+    })
+    .then(response => { 
+        if(response.error) {
+            deleteErrorMsg.innerHTML = response.error;
+        } else {
+            deleteErrorMsg.innerHTML = ''; // Show that its deleted
+        }
+    });
+});
